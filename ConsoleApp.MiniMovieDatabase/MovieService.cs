d295f434cbe1caabd841233f5a86a542351d0ff9
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-12 02:08:45 +0630

    [Complete] List To DataTable, Dumpify

--- ConsoleApp.MiniMovieDatabase/MovieService.cs
+++ ConsoleApp.MiniMovieDatabase/MovieService.cs
@@ -1,4 +1,8 @@
-﻿namespace ConsoleApp.MiniMovieDatabase;
+﻿using Dumpify;
+using System.Data;
+using System.Drawing;
+
+namespace ConsoleApp.MiniMovieDatabase;
 
 internal class MovieService
 {
@@ -23,19 +27,28 @@ internal class MovieService
         Console.Write("Enter movie title: ");
         string title = Console.ReadLine()!;
 
-        Movie movie = database.FindMovieByTitle(title);
+        //Movie movie = database.FindMovieByTitle(title);
 
-        if (movie != null)
-        {
-            Console.WriteLine($"\nMovie found:");
-            Console.WriteLine($"- Title: {movie.Title}");
-            Console.WriteLine($"- Year: {movie.Year}");
-            Console.WriteLine($"- Genre: {movie.Genre}");
-        }
-        else
+        //if (movie != null)
+        //{
+        //    Console.WriteLine($"\nMovie found:");
+        //    Console.WriteLine($"- Title: {movie.Title}");
+        //    Console.WriteLine($"- Year: {movie.Year}");
+        //    Console.WriteLine($"- Genre: {movie.Genre}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Movie not found.");
+        //}
+
+        var movies = database.FindMoviesByTitle(title);
+        if (movies.Count == 0)
         {
             Console.WriteLine("Movie not found.");
+            return;
         }
+        Console.WriteLine($"\nMovie found:");
+        ToDataTable(movies).Dump();
     }
 
     public void ListMovies(MovieDatabase database)
@@ -45,5 +58,28 @@ internal class MovieService
         {
             Console.WriteLine($"- {movie.Title} ({movie.Year}) - {movie.Genre}");
         }
+    }
+
+    public DataTable ToDataTable<T>(List<T> data)
+    {
+        DataTable table = new DataTable();
+
+        // Define columns based on object properties (assuming public getters)
+        foreach (var prop in typeof(T).GetProperties())
+        {
+            table.Columns.Add(prop.Name, prop.PropertyType);
+        }
+
+        foreach (var item in data)
+        {
+            DataRow row = table.NewRow();
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                row[prop.Name] = prop.GetValue(item);
+            }
+            table.Rows.Add(row);
+        }
+
+        return table;
     }
 }
